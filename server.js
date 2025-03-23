5f69c53c1bc898ee7c2fbce5745a9a9a1845bec7
ManalAmin <manalamin657@gmail.com>
2025-03-23 10:41:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+app.use(cors({ origin: "*" })); // For testing, allow all origins
 // Mock Database (for testing)
 let projects = [];
 
