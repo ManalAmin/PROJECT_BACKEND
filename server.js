daff0287cdd0b51b21c80f9759f71e7c991afbdb
ManalAmin <manalamin657@gmail.com>
2025-03-23 10:50:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors());
-
+app.use(cors({ origin: "https://sweet-brigadeiros-9172ea.netlify.app/" }));
 app.use(cors({ origin: "*" })); // For testing, allow all origins
 // Mock Database (for testing)
 let projects = [];
